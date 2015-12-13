9eaf2d038d1c7f7347afbd8555437d60e07ac3ef
Michael Day <contact@michaeldayreads.com>
2015-12-12 21:20:44 -0700

    Create ex2_3.py

---
+++ python_for_informatics/ex2_3.py
@@ -0,0 +1,5 @@
+# we have not covered control structures - using only what covered thus far
+hours = int(raw_input('Enter hours: '))
+rate = int(raw_input('Enter rate: '))
+pay = hours * rate
+print pay
