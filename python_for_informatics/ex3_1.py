dc8d793ecd4110101eba0d22d6e66c78d0fb3ceb
Michael Day <md@michaeldayreads.com>
2015-12-13 17:25:30 -0700

    Create ex3_1.py

---
+++ python_for_informatics/ex3_1.py
@@ -0,0 +1,7 @@
+hours = float(raw_input('Enter hours: '))  
+rate = float(raw_input('Enter rate: '))
+pay = hours * rate
+if hours > 40:
+	pay = pay + ((hours - 40) * (rate * 0.5))
+pay = float(int(pay * 100)) / 100
+print pay
