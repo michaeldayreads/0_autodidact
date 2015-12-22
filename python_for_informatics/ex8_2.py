b99178142cbb944fd7ba47d8116a29108136b200
Michael Day <md@michaeldayreads.com>
2015-12-22 14:32:53 -0700

    ex8_3.py led to improved logic

--- python_for_informatics/ex8_2.py
+++ python_for_informatics/ex8_2.py
@@ -13,15 +13,16 @@
 # 	if words[0] != 'From': continue
 # 	print words[2]
 
-# 8.2 part 2) modified to not fail test
+# 8.2 part 2) modified to not fail test, revised based on 8-3
 testfile = ['From test@test.com Sat','...','This is an arbitrary test case, but if the word were on\nits\' own line it would break it.','From John,']
 print 'The \'Test\' file:\n-----\n'
 for line in testfile:
 	print line
 print '\n-----\n'
 fhand = open('mbox.txt')
-for line in testfile:
-	words = line.split()
-	if len(words) < 3 : continue
-	if words[0] != 'From': continue
-	print words[2]
+for line in fhand:
+	if line.find('From ',0) == 0 and line.count('@') == 1:
+		words = line.split()
+		print line
+		print words[2]
+fhand.close()
