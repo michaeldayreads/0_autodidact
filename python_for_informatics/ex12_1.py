fe61edc19fcaaf86737f4decfe589a504e206a45
Michael Day <md@michaeldayreads.com>
2016-01-02 19:38:26 -0700

    corrected regex, url construction
    url.split('/') is cleaner, though testing on 12.2 showed the regex had been built for paths off the host, and seemed constructive to correct. 12.2 uses split.

--- python_for_informatics/ex12_1.py
+++ python_for_informatics/ex12_1.py
@@ -9,23 +9,27 @@ while True:
 	url = raw_input('Enter a url: ')
 	check_for_exit(url)
 	if url == '':
-		url = 'GET ' + 'http://www.dr-chuck.com/page1.htm' + ' HTTP/1.0\n\n'
+		url = 'http://www.dr-chuck.com/page1.htm'
 		print 'Using default: ', url
 	try:
-		host = re.findall('http[s]?://(.+?)/', url)[0]
-		# html = urllib.urlopen(url).read()
+		host = re.findall('http[s]?://([^/]+)', url)[0]
+		print host
+		get_url = 'GET ' + url + ' HTTP/1.0\n\n'
 		mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		mysock.settimeout(2)
 		mysock.connect((host, 80))
 		break
 	except:
 		print 'Please enter a valid url: '
 
-mysock.send(url)
+mysock.send(get_url)
+alldata = ''
 
 while True:
     data = mysock.recv(512)
+    alldata = alldata + data
     if ( len(data) < 1 ) :
         break
-    print data;
 
 mysock.close()
+print alldata
