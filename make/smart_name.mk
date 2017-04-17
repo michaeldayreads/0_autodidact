963b508df98d84ed12fb04a0e962becfcec2a606
Michael Day <m.day@f5.com>
2017-04-17 14:58:02 -0600

    Call functions with make
    Problem:
    * On a legacy Makefile, needed to find a method to use a default name unless a specific parameter was passed, in which case use a dynamically generated name. This ensured a consistency to the naming conventions used by the existing libraries to manipulate the objects in the environment stack, and also the consistency of the API and UI names the developers were used to seeing.
    
    Solution:
    * Make was a new tool. About a half day was spent reviewing online docs and tutorials, and then excerpts were used for rapid iteration on possible solutions. This was necessary to prevent time/errors involved in the actual manipulation of the obejcts in the IaaS platform the Makefile was manipulating.

--- make/smart_name.mk
+++ make/smart_name.mk
@@ -1,35 +1,38 @@
-func_testenv_name := f5mlb_func_$(orchestration)
-scale_testenv_name := f5mlb_scale_$(orchestration)
-ha_testenv_name := f5_mlb_ha_$(orchestration)
-smoke_testenv_name := f5mlb_smoke_$(orchestration)
+# sanitized and reworked excerpt testing call funtions
 
-bigip_current_ver := v12.1.0
-bigip_oldver_1_ver := v12.0.0
-bigip_oldver_1_img := bigip-osready-12.0.0.4.0.674
-bigip_oldver_2_ver := v11.6.1
-bigip_oldver_2_img := bigip-osready-11.6.1.1.0.326
+func_module_name := my_func_$(orchestration)
+scale_module_name := my_scale_$(orchestration)
+ha_module_name := f5_mlb_ha_$(orchestration)
+smoke_module_name := my_smoke_$(orchestration)
 
-bigip_ver := $(bigip_current_ver)
-ifeq ($(bigip_ver), $(bigip_current_ver))
-	bigip_params :=
-else ifeq ($(bigip_ver), $(bigip_oldver_1_ver))
-	bigip_params := --params $(bigip_testenv_name)/bigip_img:$(bigip_oldver_1_img)
-else ifeq ($(bigip_ver), $(bigip_oldver_2_ver))
-	bigip_params := --params $(bigip_testenv_name)/bigip_img:$(bigip_oldver_2_img)
+stack_current_ver := v12.1.0
+stack_oldver_1_ver := v12.0.0
+stack_oldver_1_img := stack-osready-12.0.0.4.0.674
+stack_oldver_2_ver := v11.6.1
+stack_oldver_2_img := stack-osready-11.6.1.1.0.326
+
+# stack_ver := $(stack_current_ver)
+ifeq ($(stack_ver), )
+  stack_params :=
+else ifeq ($(stack_ver), $(stack_oldver_1_ver))
+  stack_params := --params $(stack_module_name)/stack_img:$(module_oldver_1_img)
+else ifeq ($(stack_ver), $(stack_oldver_2_ver))
+  stack_params := --params $(stack_module_name)/stack_img:$(module_oldver_2_img)
 else
-	$(error ERROR: bigip version $(bigip_ver) is not supported)
+  $(error ERROR: stack version $(stack_ver) is not supported)
 endif
 
-# use 'bigip' unless there are params
-default_bigip = bigip
-named_bigip = bigip:$(1)_bigip
+# use 'stack' unless there are params
+named_stack = stack:$(1)_$(stack_ver)
 
 functest-install:
-	@echo --requires $(if $bigip_ver, \
-		$(call named_bigip,$(func_testenv_name)),bigip) $(bigip_params)
+	@echo --requires $(if $(stack_ver), \
+		$(call named_stack,$(func_module_name)),stack) $(stack_params)
 
 functest-run:
 	@echo tbd
 
 functest-teardown:
-	@echo tbd
+	$(if $(stack_ver),@echo module -v delete --name \
+		$(call named_stack,$(func_module_name)),\
+		@echo INFO: No stack_ver specified - skipping teardown)
