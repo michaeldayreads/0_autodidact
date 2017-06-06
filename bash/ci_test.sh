c5d3332691ceb5456bf5fa730ba08b7e8141d58e
Michael Day <m.day@f5.com>
2017-06-06 13:21:05 -0600

    testing logic and env vars

---
+++ bash/ci_test.sh
@@ -0,0 +1,14 @@
+#TRAVIS_REPO_SLUG="michaeldayreads/k8s-bigip-ctlr"
+#DOCKER_U="foo"
+#DOCKER_P="baz"
+
+if ["$TRAVIS_REPO_SLUG" == "F5Networks/k8s-bigip-ctlr"]; then
+    echo "docker login"
+else
+    echo "[INFO]: Not an 'F5Networks' commit, docker optional."
+    if [ "$DOCKER_P" == "" -o "$DOCKER_U" == "" ]; then
+        echo "[INFO]: DOCKER_U and/or DOCKER_P environment var absent. Add to travis to push to docker."
+    else
+        echo "[INFO]: Images will be pushed to https://hub.docker.com/u/$DOCKER_U/"
+    fi
+fi
