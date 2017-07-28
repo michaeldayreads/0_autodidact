1ed5ba5084dee52837626531e2af7300f2263613
Michael Day <m.day@f5.com>
2017-07-28 16:07:01 -0600

    bash: find sha of editable pip

--- bash/cut.sh
+++ bash/cut.sh
@@ -6,4 +6,11 @@ USER_REPO="anon/foo"
 USER=$(echo $USER_REPO | cut -d "/" -f 1)
 REPO=$(echo $USER_REPO | cut -d "/" -f 2)
 
+echo $USER_REPO
 echo "$USER and $REPO"
+
+EDITABLE_REQ="anon/foo.git@83d7a311767ea8ca47e144233c4697fce0a3a2bd"
+SHA=$(echo $EDITABLE_REQ | cut -d "@" -f 2)
+
+echo $EDITABLE_REQ
+echo $SHA
