8ff4d6d015f5b1ad1a8d9349e2b0eaa18978ffe2
Michael Day <m.day@f5.com>
2018-01-12 09:42:48 -0700

    nested ifndef when OR wanted
    makefiles do not offer an OR construct. This commit is an example of a nested ifndef that would be better expressed as an OR.
    
    Saving this pattern for future reference and to cross reference any improvements found.

---
+++ make/env_var_check.mk
@@ -0,0 +1,36 @@
+# simple starter makefile for additional new tests
+# make -f make/template.mk foo			>> ** ERROR md value not provided
+# make -f make/template.mk foo md=test		>> ** ERROR invalid value
+# make -f make/template.mk foo md=anon		>> foo
+
+
+.PHONY: all foo baz
+
+all:
+	@printf "All the things"
+
+VALID := alice anon bob
+MSG := (Valid values are alice anon bob)
+
+ifndef a
+  ifndef b
+    ifndef c
+      $(error ERROR: a, b and c have to be defined)
+    endif
+  endif
+endif
+
+
+ifndef md
+  $(error ERROR: md value not provided $(MSG))
+else ifeq ($(md), $(filter $(md), $(VALID)))
+  $(NOOP)
+else
+  $(error ERROR: invalid value '$(md)' $(VALID))
+endif
+
+foo:
+	@echo foo
+
+baz:
+	@echo baz
