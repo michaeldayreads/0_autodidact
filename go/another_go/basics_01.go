5356acfcf192f05fd3af94cdb748e4447d91b68c
Michael Day <michaeldayreads@gmail.com>
2018-04-25 11:29:12 -0600

    go study 0

---
+++ go/another_go/basics_01.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+func main() {
+    fmt.Println("A random number is", rand.Intn(10))
+    fmt.Println("\nAnd my favorite number is the empty set :P")
+}
+
+func init() {
+    rand.Seed(time.Now().UTC().UnixNano())
+    fmt.Println("\nSeeding...")
+}
