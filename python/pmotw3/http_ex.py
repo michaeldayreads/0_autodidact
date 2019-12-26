3660a14b9617f6c176deb7751939c713bc10e720
Michael Day <recursivelycurious@protonmail.com>
2019-12-25 21:52:00 -0700

    http server 0

---
+++ python/pmotw3/http_ex.py
@@ -0,0 +1,45 @@
+"""
+This file is an implementation of and response to the PMOTW on http.server.
+https://pymotw.com/3/http.server/index.html
+"""
+
+from http.server import BaseHTTPRequestHandler
+from urllib import parse
+
+class GetHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        parsed_path = parse.urlparse(self.path)
+        message_parts = [
+            'CLIENT VALUES:',
+            'client_address={} ({})'.format(
+                self.client_address,
+                self.address_string()),
+            'command={}'.format(self.command),
+            'path={}'.format(self.path),
+            'real path={}'.format(parsed_path.path),
+            'query={}'.format(parsed_path.query),
+            'request_version={}'.format(self.request_version),
+            '',
+            'SERVER VALUES:',
+            'server_version={}'.format(self.server_version),
+            'sys_version={}'.format(self.sys_version),
+            'protocal_version={}'.format(self.protocol_version),
+            '',
+            'HEADERS RECEIVED:',
+        ]
+        for name, value in sorted(self.headers.items()):
+            message_parts.append(
+                '{}={}'.format(name, value.rstrip())
+            )
+        message_parts.append('')
+        message = '\r\n'.join(message_parts)
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain; charset=utf-8')
+        self.end_headers()
+        self.wfile.write(message.encode('utf-8'))
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+    server = HTTPServer(('localhost', 8080), GetHandler)
+    server.serve_forever()
