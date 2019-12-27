0e706de4dc4ba95d9d0d5971c63327ee6119b8c4
Michael Day <recursivelycurious@protonmail.com>
2019-12-26 23:00:58 -0700

    strings 0

---
+++ python/pmotw3/string_ex.py
@@ -0,0 +1,42 @@
+"""
+This file contains code samples from, adaptations of, and response to:
+https://pymotw.com/3/string/index.html
+"""
+
+import string
+
+values = {'var': 'foo'}
+
+t = string.Template("""
+Variable        : $var
+Escape          : $$
+Variable in text: ${var}iable
+""")
+
+print('TEMPLATE:', t.substitute(values))
+
+s = """
+Variable        : %(var)s
+Escape          : %%
+Variable in text: %(var)siable
+"""
+
+print('INTERPOLATION:', s % values)
+
+s = """
+Variable        : {var}
+Escape          : {{}}
+Variable in text: {var}iable
+"""
+
+print('FORMAT:', s.format(**values))
+
+t = string.Template("$var is present, $missing is not.")
+
+try:
+    print('substitute()     :', t.substitute(values))
+except KeyError as err:
+    print('ERROR:', str(err))
+
+print('safe_substitute():', t.safe_substitute(values))
+
