af292b405c59da493a5a6fed81654bb2458b39ef
Recursively Curious <recursively.curious@protonmail.com>
2020-09-12 15:03:29 -0600

    All paths.

--- python/we/vanRossum/Guido/psf_essays/graphs.py
+++ python/we/vanRossum/Guido/psf_essays/graphs.py
@@ -18,3 +18,19 @@ def find_path(graph, start, end, path=[]):
             if newpath: return newpath
     return None
 
+
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in graph:
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    return paths
+
+
