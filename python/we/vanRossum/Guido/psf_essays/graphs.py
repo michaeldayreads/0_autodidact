df337397fccfc73932754e0c1e06fb39f5961e0e
Recursively Curious <recursively.curious@protonmail.com>
2020-09-12 18:59:29 -0600

    Breadth first search version.

--- python/we/vanRossum/Guido/psf_essays/graphs.py
+++ python/we/vanRossum/Guido/psf_essays/graphs.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 graph = {'A': ['B', 'C'],
              'B': ['C', 'D'],
              'C': ['D'],
@@ -47,4 +49,21 @@ def find_shortest_path(graph, start, end, path=[]):
                 if not shortest or len(newpath) < len(shortest):
                     shortest = newpath
     return shortest
+
+
+def find_shortest_bfs(graph, start, end):
+    dist = {start: [start]}
+    q = deque(start)
+    while len(q):
+        at = q.popleft()
+        for next in graph[at]:
+            if next not in dist:
+                dist[next] = [dist[at], next]
+                q.append(next)
+    return dist.get(end)
+
+print("--- Find path:", find_path(graph, 'A', 'D'))
+print("--- Find all paths:", find_all_paths(graph, 'A', 'D'))
+print("--- Find shortest:", find_shortest_path(graph, 'A', 'D'))
+print("--- Find shortest BFS:", find_shortest_bfs(graph, 'A', 'D'))
 
